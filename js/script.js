96973bc571e0ac0b760caf44d4c507f7ee4621a4
Ducaxias <79461791+Ducaxias@users.noreply.github.com>
2023-03-02 17:21:07 -0300

    Add files via upload
    Maior interação com formulário

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ function acao(){
     modal.style.display = 'flex'
     main.style.filter = 'blur(0.25rem)';
 }
+
+
+function fechar(){
+    let modal = document.querySelector('.modal')
+    let main = document.querySelector('main')
+
+    modal.style.display = 'none'
+    main.style.filter = 'blur(0)';
+}
