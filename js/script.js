c8113a32533ce94df3de28f18e9f6a8e4d2b0b99
Ducaxias <79461791+Ducaxias@users.noreply.github.com>
2023-03-02 17:00:42 -0300

    Add files via upload
    Adição de formulario e efeito de blur no fundo.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function acao(){
+    let modal = document.querySelector('.modal')
+    let main = document.querySelector('main')
+
+    modal.style.display = 'flex'
+    main.style.filter = 'blur(0.25rem)';
+}
